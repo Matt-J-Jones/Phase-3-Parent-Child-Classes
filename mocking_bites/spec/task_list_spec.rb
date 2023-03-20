b550e4065ce1ef820fdef27f50e78cf717c02702
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-20 14:19:15 +0000

    Unit 1 Mocking Bits Challenge Complete

--- mocking_bites/spec/task_list_spec.rb
+++ mocking_bites/spec/task_list_spec.rb
@@ -11,5 +11,30 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq false
   end
 
-  # Unit test `#all` and `#all_complete?` behaviour
+  it "adds tasks to list, returns list of tasks" do
+    task_list = TaskList.new
+    task_0 = double :task
+    task_list.add(task_0)
+    task_1 = double :task
+    task_list.add(task_1)
+    expect(task_list.all).to eq [task_0, task_1]
+  end
+
+  it "adds tasks to list, returns all tasks marked complete true" do
+    task_list = TaskList.new
+    task_0 = double :task, complete?: true
+    task_1 = double :task, complete?: true
+    task_list.add(task_0)
+    task_list.add(task_1)
+    expect(task_list.all_complete?).to eq true
+  end
+
+  it "adds tasks to list, returns all tasks marked complete false" do
+    task_list = TaskList.new
+    task_0 = double :task, complete?: true
+    task_1 = double :task, complete?: false
+    task_list.add(task_0)
+    task_list.add(task_1)
+    expect(task_list.all_complete?).to eq false
+  end
 end
